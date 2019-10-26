083517383a0f297021d67f81a301818556b83f03
arwutr <arwutr@hotmail.com>
2019-10-26 18:42:44 +0700

    Update liff-starter.js

--- liff-starter.js
+++ liff-starter.js
@@ -32,10 +32,10 @@ function initializeApp(data) {
           template: {
              type: 'image_carousel',
              columns: [{
-                 imageUrl: 'https://stickershop.line-scdn.net/stickershop/v1/sticker/159360750/IOS/sticker_popup.png',
+                 imageUrl: 'https://stickershop.line-scdn.net/stickershop/v1/sticker/'+sid'/IOS/sticker_popup.png',
                  action: {
                      type: 'uri',
-                     uri: 'line://shop/sticker/detail/13561'}}
+                     uri: 'line://shop/sticker/detail/'+pkg}}
                           ]
                         }
         }]).then(function () {
