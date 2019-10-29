da20e17fb076ebc891df5d4a29a3b1214dce31da
arwutr <arwutr@hotmail.com>
2019-10-29 22:02:22 +0700

    Update liff-starter.js

--- liff-starter.js
+++ liff-starter.js
@@ -35,7 +35,6 @@ function initializeApp(data) {
                  imageUrl: 'https://stickershop.line-scdn.net/stickershop/v1/sticker/159360750/IOS/sticker_popup.png',
                  type: 'audio',
                  originalContentUrl: 'https://stickershop.line-scdn.net/stickershop/v1/sticker/159360750/android/sticker_sound.m4a',
-                 duration: 30000
                  action: {
                      type: 'uri',
                      uri: 'line://shop/sticker/detail/13561'}}
